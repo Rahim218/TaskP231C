6c34a045e02e2f0e792d35601cb712423bcbcc07
rahimvaliyev <rahimvaliyev99@gmail.com>
2023-01-16 21:59:27 +0400

    Exercise

--- Program.cs
+++ Program.cs
@@ -1,162 +1,152 @@
-﻿#region  Array icindeki minimum elementi tapmaq
-//int[] number = { 12, 24, 1, 16, 45, 17, 23 };
-//int min = 0;
-//for (int i = 1; i < number.Length; i++)
-//{
-//    if (number[i] < number[min])
-//    {
-//        number[min] = number[i];
-//    }
-//}
-//Console.WriteLine($"En kicik reqem : {number[min]}");
-#endregion
-
-#region Arrayin uzunlugunu tapan alqoritma
-//int[] number1 = { 12, 24, 1, 16, 45, 17, 23 };
-//int count = 0;
-
-//for (int i = 0; i < number1.Length; i++)
-//{
-//    count++;
-//}
-//Console.WriteLine($"Arrayin uzunlugu : {count}");
-#endregion
-
-#region 2 ededin EBOB unu tapan alqoritma
-//int number1 = 24;
-//int number2 = 18;
-
-//int EBOB=0;
-
-
-//for (int i = 1; i < number2; i++)
-//{
-//    if (number1 % i == 0 && number2 % i == 0)
-//    {
-//        EBOB = i;
-//    }
-//}
-//Console.WriteLine($"{number1} ve {number2} - in en boyuk ortaq boluneni : {EBOB}");
-
-
-#endregion
-
-#region Verilen reqemin 2 usdunden derecesini tapan alqoritma
-//Console.WriteLine("Zehmet olmasa reqem daxil edin : ");
-//int reqem = int.Parse(Console.ReadLine());
-
-//for (int i = 0; i < 50; i++)
-//{
-//    if (reqem == Math.Pow(2, i))
-//    {
-//        Console.WriteLine($"{reqem} reqeminin 2 usdunden derecesi {i}");
-
-//    }
-//    if (reqem!= Math.Pow(2,i))
-//    {
-//        Console.WriteLine("Daxil etdiyiniz reqem 2 usdunden derecesi yoxdur");
-//        break;
-//    }
-
-
-//}
-#endregion
-
-
-
-
-#region Daxil edilen reqemin tersini yazan alqoritm
-//Console.WriteLine("Reqem daxil edin");
-//int reqem = int.Parse(Console.ReadLine());
-//int r;
-//int result = 0;
-//while (reqem != 0)
-//{
-//r = reqem % 10;
-//reqem = reqem / 10;
-//    Console.Write(r);
-//}
-
-#endregion
-
-#region Daxil edilən ədədnin rəqəmlərnin kublarının cəmini tapan alqoritm
-//Console.WriteLine("Zehmet olmasa reqem daxil edin");
-//int number = int.Parse(Console.ReadLine());
-//int n = 0;
-//double toplam = 0;
-
-//while (number != 0)
-//{
-//n = number % 10;
-//number = number / 10;
-//toplam += Math.Pow(n, 3);
-//}
-//Console.WriteLine($"Ededin reqemlerin cemi {toplam}");
-
-
-
-
-#endregion
-
-#region Daxil edilən ədədin rəqəmlərindən ən böyük olanının sayını tapan alqoritm
-Console.WriteLine("Zehmet olmasa 6 reqemli eded daxil edin :");
-int number = int.Parse(Console.ReadLine());
-int[] n = new int[6];
-int i = 0;
-while (number != 0)
+﻿namespace Taskkkk
 {
-
-    n[i] = number % 10;
-    number = number / 10;
-    i++;
-
-}
-
-int max = 0;
-int count = 0;
-for (int j = 1; j < n.Length; j++)
-{
-    if (n[j] > n[max])
+    internal class Program
     {
-        n[max] = n[j];
+        static void Main(string[] args)
+        {
+            #region Arrayin icindeki cut ededlerin ortalamasi
+            //int[] numbers = { 24, 15, 17, 64, 10, 11 };
 
+            //int sum = 0;
+            //double count = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i]%2==0)
+            //    {
+            //        sum += numbers[i];
+            //        count++;
+            //    }
+            //}
+            //double avarage = sum/count;
+
+            //Console.WriteLine(avarage);
+            #endregion
+
+            #region Array icinde en boyuk ededi tapmaq
+            //int[] numbers = { 15, 25, 11, 9, 45, 28, 96 };
+            //int max = numbers[0];
+            //for (int i = 1; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] > max)
+            //    {
+            //        max = numbers[i];
+            //    }
+
+            //}
+            //Console.WriteLine(max);
+
+            #endregion
+
+            #region Verilmis 3 ededin en boyuyunu tapmaq
+            //Console.Write("1 ci ededi daxil edin : ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.Write("2 ci ededi daxil edin : ");
+            //int num2 = int.Parse(Console.ReadLine());
+            //Console.Write("3 cu ededi daxil edin : ");
+            //int num3 = int.Parse(Console.ReadLine());
+
+            //if (num1 >= num2 && num1 >= num3)
+            //{
+            //    Console.WriteLine($"En boyuk eded {num1}");
+            //}
+            //else if (num2 >= num1 && num2 >= num3)
+            //{
+            //    Console.WriteLine($"En boyuk ededn {num2}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"En boyuk reqem {num3}");
+            //}
+
+
+            #endregion
+
+            #region Verilmis 3 ededin en kiciyini tapin
+            //Console.Write("1 ci ededi daxil edin : ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.Write("2 ci ededi daxil edin : ");
+            //int num2 = int.Parse(Console.ReadLine());
+            //Console.Write("3 cu ededi daxil edin : ");
+            //int num3 = int.Parse(Console.ReadLine());
+
+            //if (num1 <= num2 && num1 <= num3)
+            //{
+            //    Console.WriteLine($"En kicik eded {num1}");
+            //}
+            //else if (num2 <= num1 && num2 <= num3)
+            //{
+            //    Console.WriteLine($"En kicik ededn {num2}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"En kicik reqem {num3}");
+            //}
+
+            #endregion
+
+            #region Consoldan gelen ededleri arrayin icine elave eden alqoritma
+            //int[] numbers = new int[6];
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.Write($"{i+1} ci reqemi daxil edin : ");
+            //    int getNumber = int.Parse(Console.ReadLine());
+            //    numbers[i] = getNumber;
+
+
+            //}
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+
+            #endregion
+
+
+            #region Arrayin icinde 3 bolunen reqemlerin ededi ortasini tapan alqoritma
+            //int[] numbers = { 33, 10, 12, 64, 27, 11 };
+
+            //int sum = 0;
+            //double count = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 3 == 0)
+            //    {
+            //        sum += numbers[i];
+            //        count++;
+            //    }
+            //}
+            //double avarage = sum / count;
+
+            //Console.WriteLine(avarage);
+            #endregion
+
+            #region Arrayin icinde 3 reqemli eded olub olmadigini yoxlayan proqram
+            int[] numbers = { 21, 123, 25, 456, 12, 65, 1000 };
+            string numbersStr;
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbersStr = Convert.ToString(numbers[i]);
+                if (numbersStr.Length==3)
+                {
+                    count++;
+                }
+            }
+            if (count != 0)
+            {
+                Console.WriteLine($"Arrayin icinde {count} dene 3 reqemli eded var");
+
+            }
+            else
+            {
+                Console.WriteLine("Arrayin icinde 3 reqemli eded yoxdur");
+            }
+            
+            #endregion
+        }
     }
-    if (n[j] == n[max])
-    {
-        count++;
-    }
-
 }
-Console.WriteLine($"En boyuk reqem {n[max]}");
-Console.WriteLine($"Eyni reqemlerin sayi {count}");
-
-
-#endregion
-
-
-#region MyRegion
-
-
-
-int number = int.Parse(Console.ReadLine());
-
-string s = Convert.ToString(number);
-int count = 0;
-for (int i = 0; i < s.Length; i++)
-{
-    count++;
-}
-if (count == 3)
-{
-    int result = (3000 + number) * 10 + 3;
-
-    Console.WriteLine(result);
-}
-else
-{
-    Console.WriteLine("Zehmet olmasa 3 reqemli eded daxil edin");
-}
-
-
-
-#endregion
